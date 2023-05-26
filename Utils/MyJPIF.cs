d7eb53f340658c887e7bda40fb9613d0fbc3693d
Henrique Almeida <henriquealmeida@Henriques-MacBook-Pro.local>
2023-05-26 00:06:15 -0300

    feat: created patch route to partial update

---
+++ Utils/MyJPIF.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
+
+namespace nosso_portifolio_api.Utils
+{
+    public static class MyJPIF
+    {
+        public static NewtonsoftJsonPatchInputFormatter GetJsonPatchInputFormatter()
+        {
+            var builder = new ServiceCollection()
+                .AddLogging()
+                .AddMvc()
+                .AddNewtonsoftJson()
+                .Services.BuildServiceProvider();
+
+            return builder
+                .GetRequiredService<IOptions<MvcOptions>>()
+                .Value
+                .InputFormatters
+                .OfType<NewtonsoftJsonPatchInputFormatter>()
+                .First();
+        }
+    }
+}
+
